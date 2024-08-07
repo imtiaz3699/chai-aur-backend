5ae8c3574161b404138b5fbf387dd000266af9ba
Imtiaz Ahmed <imtiazali78652@gmail.com>
2024-08-07 13:14:34 +0500

    last

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,9 @@
 import { asyncHanlder } from "../utils/asyncHandler.js";
 const registerUser = asyncHanlder(async(req,res)=> {
-    res.status(200).json({message:'chai aur code'})
+    // get user data from front-end
+    // validation
+    // not empty
+    // check if user already exists
+    // res.status(200).json({message:'chai aur code'})
 });
 export{registerUser};
